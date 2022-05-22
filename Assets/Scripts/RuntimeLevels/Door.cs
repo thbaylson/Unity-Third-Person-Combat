2728dbcf41868c3cf63ea7fb3bfe1e10ae126f08
Tyler Baylson <thbaylson@gmail.com>
2022-05-22 14:02:23 -0400

    Doors now have an animation for opening. Changed folder structure.

--- Assets/Scripts/RuntimeLevels/Door.cs
+++ Assets/Scripts/RuntimeLevels/Door.cs
@@ -8,37 +8,22 @@ namespace ThirdPersonCombat.RuntimeLevels
 {
     public class Door : MonoBehaviour
     {
-        [SerializeField] TextMeshPro textMeshPro;
-        [SerializeField] TextMeshPro textMeshPro2;
-        [SerializeField] bool isLocked = true;
-
-        // Doors need two colliders. One as a trigger and one as a non-trigger so players can't move through them
-        BoxCollider doorCollider;
+        Animator stateAnimator;
 
         // Awake is called before Start. Initialize properies here
         private void Awake()
         {
-            // Grab the collider that prevents the player from moving through this object
-            doorCollider = GetComponents<BoxCollider>().Where(c => !c.isTrigger).First();
-        }
-
-        void Update()
-        {
-            if (textMeshPro != null)
-            {
-                textMeshPro.text = (isLocked) ? "Locked Door" : "Open Door";
-                textMeshPro2.text = (isLocked) ? "Locked Door" : "Open Door";
-                doorCollider.enabled = isLocked;
-
-                GetComponent<MeshRenderer>().enabled = isLocked;
-            }
+            stateAnimator = GetComponent<Animator>();
         }
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player") && other.GetComponent<PlayerInventory>().HasKeys())
             {
-                isLocked = false;
+                // If animator isn't hooked up, return
+                if (stateAnimator.Equals(null)) return;
+                
+                stateAnimator.SetBool("isClosed", false);
             }
         }
     }
